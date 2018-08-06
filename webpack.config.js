62ea905989bc0f6d3efc465a3398459b84aabc55
Mikayel Amirkhanyan <mikayel@mikayel.com>
2018-08-06 13:43:09 -0400

    Webpack and react versions update

--- webpack.config.js
+++ webpack.config.js
@@ -64,23 +64,6 @@ if (process.env.NODE_ENV === 'production') {
       new webpack.LoaderOptionsPlugin({
         minimize: true,
         debug: false
-      }),
-      new webpack.optimize.UglifyJsPlugin({
-        compress: {
-            warnings: false,
-            screw_ie8: true,
-            conditionals: true,
-            unused: true,
-            comparisons: true,
-            sequences: true,
-            dead_code: true,
-            evaluate: true,
-            if_return: true,
-            join_vars: true
-        },
-        output: {
-            comments: false
-        },
       })
     );
 }
