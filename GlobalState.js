b268d36311e27766defbeae425b9171ca7079a0a
Mikayel <mikayel@mikayel.com>
2019-01-17 09:45:29 -0500

    removed function name

--- GlobalState.js
+++ GlobalState.js
@@ -5,7 +5,7 @@ import React from 'react'
 const GlobalStateContext = React.createContext({globalState: {}, setGlobalState: () => {} });
 
 export function withGlobalState(Component) {
-    return function WrapperComponent(props) {
+    return (props) => {
         return (
             <GlobalStateContext.Consumer>
                 {(contexts) => <Component {...props} {...contexts} />}
